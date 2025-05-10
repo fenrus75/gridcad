4f8b5522b895614221d6f8c479db16c21693aae0
Arjan van de Ven <arjanvandeven@gmail.com>
2025-05-10 14:00:19 -0700

    set renderquality scaling

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 {
     class scene *_scene;
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
     
     _scene = new scene();
     
